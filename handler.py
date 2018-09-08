c8f4a837a35f730c45a7bf8cf576673677acf24b
Mikio Tsunematsu <tsunematsum@gmail.com>
2018-09-08 20:44:28 +0100

    Replace default function with string_transformer
    Replace the default Serverless 'hello' function with a string_transformer.

--- handler.py
+++ handler.py
@@ -1,9 +1,23 @@
 import json
+import re
 
+def string_transformer(event, context):
 
-def hello(event, context):
+    # Get the strings out of the dict and into more convenient variables
+    prefix_string = event['prefix_string']
+    object_string = event['object_string']
+    # Get the substring that stops at the first vowel
+    devoweled_prefix_array = re.split('[AEIOU]', prefix_string, maxsplit=1, flags=re.IGNORECASE)[0]
+    # Convert it back into a string
+    devoweled_prefix_string = ''.join(devoweled_prefix_array)
+    # Strip letters from the object string until we hit our first vowel
+    strings_to_delete_from_object = re.split('[AEIOU]', object_string, maxsplit=1, flags=re.IGNORECASE)[0]
+    devoweled_object_string = re.sub(strings_to_delete_from_object, '', object_string)
+    # Ellide/Concatenate the strings
+    concatenated_string = devoweled_prefix_string.title() + devoweled_object_string.lower()
+
     body = {
-        "message": "Go Serverless v1.0! Your function executed successfully!",
+        "message": concatenated_string,
         "input": event
     }
 
@@ -13,12 +27,3 @@ def hello(event, context):
     }
 
     return response
-
-    # Use this code if you don't use the http event with the LAMBDA-PROXY
-    # integration
-    """
-    return {
-        "message": "Go Serverless v1.0! Your function executed successfully!",
-        "event": event
-    }
-    """
